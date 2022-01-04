f8679d088e046140940875ec085159a7bb4325b5
Nadi Awad <nadiawad60@hotmail.com>
2022-01-04 17:20:46 +0000

    Day-1 Test the input function

--- day-1-hello-world.py
+++ day-1-hello-world.py
@@ -1,2 +1,6 @@
 print("Hello World!")
 print("Hello World one more time!\nHello World again!")
+print("Hello" + " " + "Nadi!")
+
+name = input("What is your name?")
+print("The number of characters in your name is: " + str(len(name)))
