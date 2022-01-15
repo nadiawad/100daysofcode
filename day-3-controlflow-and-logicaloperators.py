63ac32087b4b71584a1ae6616118b820d1818889
Nadi Awad <nadiawad60@hotmail.com>
2022-01-15 10:54:47 +0000

    Day-3 if-statement.

---
+++ day-3-controlflow-and-logicaloperators.py
@@ -0,0 +1,24 @@
+print("Welcome to my Rollercoaster!")
+height = int(input("Enter your height in cm: "))
+
+if height >= 120:
+    print("You can play, YAY!")
+else:
+    print("Sorry :( You cannot play!")
+
+
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+bmi = round(weight / (height ** 2))
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi >= 35:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
