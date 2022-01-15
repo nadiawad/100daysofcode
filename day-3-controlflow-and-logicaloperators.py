6871c06e93db1174dcc9fe37ad99dfa1667bbb83
Nadi Awad <nadiawad60@hotmail.com>
2022-01-15 19:39:56 +0000

    "Day-3 if-statement. Pizza Example."

--- day-3-controlflow-and-logicaloperators.py
+++ day-3-controlflow-and-logicaloperators.py
@@ -48,3 +48,23 @@ elif bmi < 35:
 elif bmi >= 35:
     print(f"Your BMI is {bmi}, you are clinically obese.")
 
+
+print("Welcome to Nadi's Pizza Shop!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
