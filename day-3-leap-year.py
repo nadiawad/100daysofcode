043d79b7c8a471a2524fe8496ba7070ac09388e7
Nadi Awad <nadiawad60@hotmail.com>
2022-01-15 13:12:34 +0000

    Day-3 if-statement. Leap year example.

--- day-3-leap-year.py
+++ day-3-leap-year.py
@@ -7,7 +7,5 @@ year = int(input("Enter a year to check if it is a leap year: "))
 if year % 4 == 0:
     if year % 100 != 0 or (year % 100 == 0 and year % 400 == 0):
         print("Leap year.")
-    else:
-        print("Not leap year.")
 else:
     print("Not leap year.")
